487a648f2d359f6564080d34d81966c7e83af975
Ard Scheirlynck <ardscheirlynck@Ards-MacBook-Pro-2.local>
2018-11-02 00:10:53 +0100

    fix typo

--- src/pages/blog/my-first-blog-post/index.md
+++ src/pages/blog/my-first-blog-post/index.md
@@ -4,9 +4,9 @@ date: "2018-10-25T21:40:00.000Z"
 type: Blogpost
 ---
 
-Ok, this is quite exciting to me; my first (ever) blogpost. 
+Ok, this is quite exciting to me; my first (ever) blog post. 
 
-A while back I decided it was time to step up my game and express my thoughts, feelings and new discoveries in a blog. The best part of it? All of this is a blog I'm developing and mainting myself. After two years of development experience I feel quite confident enough to try something out like this; so here I am!
+A while back I decided it was time to step up my game and express my thoughts, feelings and new discoveries in a blog. The best part of it? All of this is a blog I'm developing and maintaining myself. After two years of development experience I feel quite confident enough to try something out like this; so here I am!
 
 In the [WhoAmI](/whoami/) part of my blog, you can discover a bit more about me. In these blog series, I will try to weekly post something new about tech, games or some event in my life. As I'm a developer, changes are rather big that in this process the blog itself will change as well. If you like my blog, would like to discuss something or just want to drink a coffee, feel free to reach out!
 
--- src/pages/whoami.js
+++ src/pages/whoami.js
@@ -45,7 +45,7 @@ class WhoAmiI extends React.Component {
         <p>
           Hi there!<br/>
           My name is Ard Scheirlynck. Allow me to introduce myself.<br/><br/>
-          At this very moment I'm living in Gullegem, Belgium. I'm happily living together with my girlfriend and my awesome dog Nalu.
+          Hi, my name is Ard and currently I'm living in Gullegem, Belgium. I'm happily living together with my girlfriend and my awesome dog Nalu.
           At day I'm working at Melexis as an IT Business Analyst/Developer for the Core Business Solutions team. When I'm behind my desk
           I'm developing awesome applications, analyzing the business needs and deploying/maintaining my baby applications. 
           Most of the time I'm using Java, Python, Docker, Kubernetes to achieve my goals but I'm keen on experimenting with other languages and technologies.<br/><br/>
@@ -53,7 +53,7 @@ class WhoAmiI extends React.Component {
           At night I like spending time with my 3 months old dog Nalu and my awesome girlfriend. When I'm not playing with one of them, I probably have a PlayStation 4-controller 
           in my hands or I'm sitting behind my desk in my mancave. When I'm not working for Melexis, I'm always bizzy experimenting with cool technologies like my Raspberry Pi or
           cool tech like Jekyll, GatsbyJS... Gaming is also a big passion of mine; therefore I'm also an editor for <a href="https://www.9lives.be/">9lives.be</a> where 
-          I write reviews and previews for cool videogames. At last but not least, sports is also a big part of my life. You can find me on the basketball court 3 to 4 times per week.
+          I write reviews and previews for cool video games. At last but not least, sports is also a big part of my life. You can find me on the basketball court 3 to 4 times per week.
           When not playing basketball, I like to ride my racing bike or go jogging with my lovely girlfriend. <br/><br/>
 
         </p> 
